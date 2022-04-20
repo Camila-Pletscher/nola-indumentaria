8be9446f7d4ad529cbc376b6448daa232d45a17d
Cami <camipletscher@gmail.com>
2022-04-20 19:10:30 -0300

    2da pre entrega
    dom + storage

--- src/agregarAlCarrito.js
+++ src/agregarAlCarrito.js
@@ -2,59 +2,41 @@ import { productos } from "./stock.js";
 
 let carritoDePedido = [];
 
-const productosDelStorage = JSON.parse(localStorage.getItem("productosAgregados"));
-// console.log(productosDelStorage);
-if (productosDelStorage != null) {
-  carritoDePedido = productosDelStorage;
-}
+carritoDePedido = JSON.parse(localStorage.getItem("productosAgregados")) || [];
 
 console.log(carritoDePedido);
 
-carritoDePedido.forEach(producto => {
-
+carritoDePedido.forEach((producto) => {
   agregarProductoAlModal(producto);
-  const btnEliminar = document.getElementById(`eliminar-producto${producto.codigo}`);
-  btnEliminar.addEventListener("click", () => {
-    localStorage.removeItem(`${producto.codigo}`)
-  });
-
+  
 });
 
+
+
 export const agregarAlCarrito = (productoCodigo) => {
+  
   const encontrarProductos = () => {
     let producto = productos.find(
       (producto) => producto.codigo == productoCodigo
     );
-
+  
     carritoDePedido.push(producto);
     console.log(carritoDePedido);
-
+  
     producto.cantidad = 1;
-
+  
     agregarProductoAlModal(producto);
-
-
-
-    // for (const productosStorage of carritoDePedido) {
-    //   guardarLocal(productosStorage.codigo, JSON.stringify(productosStorage));
-    // }
-    guardarLocal('productosAgregados', JSON.stringify(carritoDePedido));
-
+  
+    localStorage.setItem("productosAgregados", JSON.stringify(carritoDePedido));
   };
 
+  
 
   encontrarProductos();
-
-
-
 };
 
-const guardarLocal = (clave, valor) => {
-  localStorage.setItem(clave, valor);
-}
-
 function agregarProductoAlModal(producto) {
-  let modalBody = document.querySelector('.modal-body');
+  let modalBody = document.querySelector(".modal-body");
   const div = document.createElement("div");
   div.innerHTML += `<p>${producto.nombre}</p>
                             <p>Precio: ${producto.precio}</p>
@@ -62,11 +44,8 @@ function agregarProductoAlModal(producto) {
                             <p id="cantidad${producto.codigo}">Cantidad: ${producto.cantidad}</p>
                             <button id="eliminar-producto${producto.codigo}">
                             <i class="small material-icons">delete</i>
-                            </button>`
+                            </button>`;
   modalBody.appendChild(div);
-
-};
-
-
+}
 
 
