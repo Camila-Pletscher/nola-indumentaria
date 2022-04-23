1e137e6c8420a8b4018bc50d2066917b78377760
Cami <camipletscher@gmail.com>
2022-04-23 02:54:26 -0300

    Problemas con la cantidad
    se crea un div de mas en el modal

--- src/agregarAlCarrito.js
+++ src/agregarAlCarrito.js
@@ -2,50 +2,124 @@ import { productos } from "./stock.js";
 
 let carritoDePedido = [];
 
-carritoDePedido = JSON.parse(localStorage.getItem("productosAgregados")) || [];
+//Valido si en el storage hay algo y sino le asigno un array vacio
+carritoDePedido = JSON.parse(localStorage.getItem("productosAgregados")) ?? [];
 
 console.log(carritoDePedido);
 
-carritoDePedido.forEach((producto) => {
-  agregarProductoAlModal(producto);
-  
-});
-
+class Producto {
+  constructor(codigo, nombre, precio, img, stock, cantidad) {
+    this.codigo = codigo
+    this.nombre = nombre
+    this.precio = precio
+    this.img = img
+    this.stock = stock
+    this.cantidad = cantidad
+  }
+}
 
 
 export const agregarAlCarrito = (productoCodigo) => {
   
   const encontrarProductos = () => {
+    //Busca el producto entre el stock
     let producto = productos.find(
       (producto) => producto.codigo == productoCodigo
     );
-  
+    //Busco si esta repetido 
+    let productoRepetido = carritoDePedido.find((producto) => producto.codigo == productoCodigo);
+    
+    if (sumarProductoRepetido(productoRepetido) && (productoRepetido = producto)) {
+      producto.cantidad = 1;
+    }
+    //Ejecuto fx para ver sumar si esta repetido o eliminar
+    
+
+    let codigo = producto.codigo;
+    let nombre = producto.nombre;
+    let precio = producto.precio;
+    let img = producto.img;
+    let stock = producto.stock;
+    let cantidad = producto.cantidad;
+    //Instancio y creo un nuevo objeto con esos datos
+    producto = new Producto(codigo, nombre, precio, img, stock, cantidad);
+    //Lo agrego al carrito
     carritoDePedido.push(producto);
+    
     console.log(carritoDePedido);
-  
-    producto.cantidad = 1;
-  
+    //Lo guardo en el storage
+    localStorage.setItem("productosAgregados", JSON.stringify(carritoDePedido));
+    
+    //Agrego el producto al modal del carrito
     agregarProductoAlModal(producto);
   
-    localStorage.setItem("productosAgregados", JSON.stringify(carritoDePedido));
   };
-
-  
 
   encontrarProductos();
 };
 
-function agregarProductoAlModal(producto) {
-  let modalBody = document.querySelector(".modal-body");
-  const div = document.createElement("div");
-  div.innerHTML += `<p>${producto.nombre}</p>
-                            <p>Precio: ${producto.precio}</p>
+function sumarProductoRepetido (prodRepetido) {
+  if (prodRepetido) {
+    carritoDePedido = carritoDePedido.filter(item => item.codigo != prodRepetido.codigo);
+    console.log(carritoDePedido);
+    prodRepetido.cantidad++
+    carritoDePedido.push(prodRepetido);
+    localStorage.setItem("productosAgregados", JSON.stringify(carritoDePedido));
+    console.log(prodRepetido.cantidad);
+    document.getElementById(`cantidad${prodRepetido.codigo}`).innerHTML = `<p id="cantidad${prodRepetido.codigo}">Cantidad: ${prodRepetido.cantidad}</p>`;
+    
+    return prodRepetido;
+    }
+}
 
+function agregarProductoAlModal() {
+  let modalBody = document.querySelector(".modal-body");
+  modalBody.innerHTML = "";
+  carritoDePedido.forEach(producto => {
+    let div = document.createElement("div");
+    div.innerHTML = `<p>${producto.nombre}</p>
+                            <p>Precio: ${producto.precio}</p>
                             <p id="cantidad${producto.codigo}">Cantidad: ${producto.cantidad}</p>
-                            <button id="eliminar-producto${producto.codigo}">
-                            <i class="small material-icons">delete</i>
-                            </button>`;
-  modalBody.appendChild(div);
+                            `;
+
+    const botonBorrar = document.createElement("button");
+    botonBorrar.innerHTML += `<i class="small material-icons">delete</i>`
+    botonBorrar.addEventListener('click', () => {
+      eliminarProductoDelCarrito(producto);
+
+
+    })
+    div.appendChild(botonBorrar);
+
+    modalBody.appendChild(div);
+
+
+  })
+
+
+};
+
+function eliminarProductoDelCarrito(producto) {
+  Swal.fire({
+    title: 'Are you sure?',
+    text: "You won't be able to revert this!",
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonColor: '#3085d6',
+    cancelButtonColor: '#d33',
+    confirmButtonText: 'Yes, delete it!'
+  }).then((result) => {
+    if (result.isConfirmed) {
+      carritoDePedido = carritoDePedido.filter(item => item.codigo != producto.codigo);
+      localStorage.setItem("productosAgregados", JSON.stringify(carritoDePedido));
+      agregarProductoAlModal();
+      Swal.fire(
+        'Deleted!',
+        'Your file has been deleted.',
+        'success'
+      )
+    }
+  })
 }
 
 
--- src/stock.js
+++ src/stock.js
@@ -5,7 +5,7 @@ const productos = [
     precio: 4500,
     img: './public/images/buzo-rey-leon.png',
     stock: 10,
-    cantidad: 1
+    cantidad: ''
   }, 
 
   {
@@ -14,7 +14,7 @@ const productos = [
     precio: 4200,
     img: './public/images/buzo-mulan.png',
     stock: 10,
-    cantidad: 1
+    cantidad: ''
   },
 
   {
@@ -23,7 +23,7 @@ const productos = [
     precio: 4300,
     img: './public/images/buzo-toy-story.png',
     stock: 10,
-    cantidad: 1
+    cantidad: ''
   },
 
   {
@@ -32,7 +32,7 @@ const productos = [
     precio: 4250,
     img: './public/images/buzo-aladdin.png',
     stock: 10,
-    cantidad: 1
+    cantidad: ''
   },
 
   {
@@ -41,7 +41,7 @@ const productos = [
     precio: 4100,
     img: './public/images/buzo-sailor-moon.png',
     stock: 10,
-    cantidad: 1
+    cantidad: ''
   },
 
   {
@@ -50,7 +50,7 @@ const productos = [
     precio: 2100,
     img: './public/images/remera-friends.png',
     stock: 10,
-    cantidad: 1
+    cantidad: ''
   },
 
   {
@@ -59,7 +59,7 @@ const productos = [
     precio: 2200,
     img: './public/images/remera-juego-gemelas.png',
     stock: 10,
-    cantidad: 1
+    cantidad: ''
   },
 
   {
@@ -68,7 +68,7 @@ const productos = [
     precio: 2350,
     img: './public/images/remera-chandler-mood.png',
     stock: 10,
-    cantidad: 1
+    cantidad: ''
   },
 
   {
@@ -77,7 +77,7 @@ const productos = [
     precio: 2500,
     img: './public/images/remera-pantone-mushu.png',
     stock: 10,
-    cantidad: 1
+    cantidad: ''
   },
 
   {
@@ -86,7 +86,7 @@ const productos = [
     precio: 2400,
     img: './public/images/remera-hakuna-matata.png',
     stock: 10,
-    cantidad: 1
+    cantidad: ''
   },
 ];
 
